691ce9cb004f07608354719d850a735e4c3c3d55
Ian Andrewson <ianandrewson@gmail.com>
2020-02-03 15:13:31 -0800

    test written for fetch. mock api call implemented. test passes

--- src/containers/FetchFuture.js
+++ src/containers/FetchFuture.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Header from '../components/header/Header.js';
 import QuoteList from '../components/quoteList/QuoteList.js';
+import getQuotes from '../services/FuturamaApi.js';
 
 export default class FetchFuture extends Component {
+
+  state = {
+    quotes: [
+      { character: 'Ian', quote: 'Just to see this works' },
+      { character: 'Someone Else', quote: 'A second test' }
+    ]
+  }
+
+  fetch = () => {
+    return getQuotes()
+      .then(result => this.setState({ quotes: result }));
+  }
+
   render() {
+    const { quotes } = this.state;
+
     return (
       <>
         <Header />
-        <QuoteList />
+        <QuoteList quotes={quotes} />
       </>
     );
   }
