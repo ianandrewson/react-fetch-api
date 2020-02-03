1c6f3946aaa4d99fcb67a618b38713b146674ed0
Ian Andrewson <ianandrewson@gmail.com>
2020-02-03 13:52:38 -0800

    FuturamaQuote and QuoteList components implemented. Ready to write fetch functions and set in state

--- src/components/quote/FuturamaQuote.js
+++ src/components/quote/FuturamaQuote.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function FuturamaQuote() {
+export default function FuturamaQuote({ character, quote }) {
   return (
-    <p>This is a single Quote. Render from props</p>
+    <>
+      <p>Character: {character}</p>
+      <p>Quote: {quote}</p>
+    </>
   );
 }
+
+FuturamaQuote.propTypes = {
+  character: PropTypes.string.isRequired,
+  quote: PropTypes.string.isRequired
+};
+
--- src/components/quoteList/QuoteList.js
+++ src/components/quoteList/QuoteList.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FuturamaQuote from '../quote/FuturamaQuote.js';
 
-export default function QuoteList() {
+export default function QuoteList({ quotes }) {
+
+  const fetchedQuotes = quotes.map(({ quote, character }, i) => {
+    return (
+      <li key={i}>
+        <FuturamaQuote character={character} quote={quote}/>
+      </li>
+    );
+  });
+
   return (
-    <>
-      <p>Quotes will go here. Get quote data from api fetch set in state.</p>
-      <FuturamaQuote />
-    </>
+    <ul>
+      {fetchedQuotes}
+    </ul>
   );
 }
+
+QuoteList.propTypes = {
+  quotes: PropTypes.arrayOf(PropTypes.shape({
+    quote: PropTypes.string.isRequired,
+    character: PropTypes.string.isRequired
+  })).isRequired
+};
